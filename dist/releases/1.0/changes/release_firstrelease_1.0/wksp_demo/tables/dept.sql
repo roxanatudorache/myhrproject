522fe2659bd8f3e5cf68469f0dd26d570bef0fa6
roxanatudorache <roxana.tudorache89@gmail.com>
2026-04-23 11:05:32 +0000

    feat: committing changelogs in the dist folder for release 1.0

---
+++ dist/releases/1.0/changes/release_firstrelease_1.0/wksp_demo/tables/dept.sql
@@ -0,0 +1,14 @@
+-- liquibase formatted sql
+-- changeset WKSP_DEMO:1776942274205 stripComments:false  logicalFilePath:release_firstrelease_1.0/wksp_demo/tables/dept.sql
+-- sqlcl_snapshot src/database/wksp_demo/tables/dept.sql:null:245e7ca8d45d1c55cfaaf9874b7de077b85dac5f:create
+
+create table dept (
+    id              number default on null to_number(sys_guid(), 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') not null enable,
+    department_name varchar2(255 char),
+    location        varchar2(4000 char)
+);
+
+alter table dept
+    add constraint dept_id_pk primary key ( id )
+        using index enable;
+
