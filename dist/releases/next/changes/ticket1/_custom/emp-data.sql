0740a4cd2178caf51bbab81613348b5b5dcda263
roxanatudorache <roxana.tudorache89@gmail.com>
2026-04-23 11:21:09 +0000

    TICKET-1: data files custom

---
+++ dist/releases/next/changes/ticket1/_custom/emp-data.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+-- changeset  SqlCl:1776943235209 stripComments:false logicalFilePath:_custom/emp-data.sql
+-- sqlcl_snapshot dist/releases/next/changes/ticket1/_custom/emp-data.sql:null:null:custom
+
+
+insert into emp values( 7839, 'KING', 'MICHAEL', 'K.M@TEST',  10);
+insert into emp values( 7698, 'BLAKE', 'JOHN', 'B.J@TEST',  30);
+insert into emp values( 7782, 'CLARK', 'DOE', 'C.D@TEST',  10);
+insert into emp values( 7566, 'JONES', 'ABBOT', 'J.A@TEST',  20);
+
