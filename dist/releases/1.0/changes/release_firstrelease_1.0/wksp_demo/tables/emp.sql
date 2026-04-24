d51d2415073d33ebebd321590844edabad612552
roxanatudorache <roxana.tudorache89@gmail.com>
2026-04-24 13:42:51 +0000

    feat: committing changelogs in the dist folder for release 1.0

---
+++ dist/releases/1.0/changes/release_firstrelease_1.0/wksp_demo/tables/emp.sql
@@ -0,0 +1,16 @@
+-- liquibase formatted sql
+-- changeset WKSP_DEMO:1777038126683 stripComments:false  logicalFilePath:release_firstrelease_1.0/wksp_demo/tables/emp.sql
+-- sqlcl_snapshot src/database/wksp_demo/tables/emp.sql:null:3aae2355c1a61c15d9a96d1dd4c3e077353fc7bf:create
+
+create table emp (
+    id         number default on null to_number(sys_guid(), 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') not null enable,
+    first_name varchar2(255 char),
+    last_name  varchar2(255 char),
+    email      varchar2(255 char),
+    dep_id     number
+);
+
+alter table emp
+    add constraint emp_id_pk primary key ( id )
+        using index enable;
+
